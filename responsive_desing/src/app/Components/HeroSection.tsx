ca6b203a9c368fab9fe0058cf4e0eae60d016bda
noor ul <hudanoorul714@gmail.com>
2024-11-23 23:37:11 +0500

    responsive desing

---
+++ responsive_desing/src/app/Components/HeroSection.tsx
@@ -0,0 +1,15 @@
+
+
+export default function HeroSection() {
+    return (
+        <div>
+            <h1 className="text-green-700 
+            md:text-yellow-600
+            lg:text-gray-700
+            text-center font-extrabold text-4xl md:text-6xl lg:text-9xl
+            ">
+                Responsive Desing
+            </h1>
+        </div>
+    )
+}
---
+++ responsive_desing/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import HeroSection from "./Components/HeroSection";
+import FeaturedProduct from "./Components/FeaturedProduct";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <FeaturedProduct />
+    </div>
+  );
+}
